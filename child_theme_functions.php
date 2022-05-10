d04c1650985959a039995c5dfad78dafe30562a0
MD AL AMIN <52990610+1mdalamin1@users.noreply.github.com>
2022-05-10 08:41:03 +0600

    Update child_theme_functions.php

--- child_theme_functions.php
+++ child_theme_functions.php
@@ -6,4 +6,5 @@ function example_theme_support() {
 }
 add_action( 'after_setup_theme', 'example_theme_support' );
 
-
+// post old editor || default editor
+add_filter('use_block_editor_for_post', '__return_false', 10);
